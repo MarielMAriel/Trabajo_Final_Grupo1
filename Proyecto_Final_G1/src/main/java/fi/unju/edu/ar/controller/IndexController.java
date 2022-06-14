4101fae72f56950640296fda22fe3f680ef2d1ca
gusta <gusta@LAPTOP-P47OBGCS>
2022-06-14 20:44:57 -0300

    Agregue notacion repository

--- Proyecto_Final_G1/src/main/java/fi/unju/edu/ar/controller/IndexController.java
+++ Proyecto_Final_G1/src/main/java/fi/unju/edu/ar/controller/IndexController.java
@@ -4,7 +4,7 @@ package fi.unju.edu.ar.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import fi.unju.edu.ar.entity.Empleado;
+
 
 @Controller
 public class IndexController {
--- Proyecto_Final_G1/src/main/java/fi/unju/edu/ar/controller/UsuarioController.java
+++ Proyecto_Final_G1/src/main/java/fi/unju/edu/ar/controller/UsuarioController.java
@@ -3,17 +3,18 @@ package fi.unju.edu.ar.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import fi.unju.edu.ar.entity.Empleado;
 import fi.unju.edu.ar.service.IEmpleadoService;
-
+@Controller
 public class UsuarioController {
 	@Autowired
-	IEmpleadoService empleadoService;
-	Empleado empleado =new Empleado() ;
+	private IEmpleadoService empleadoService;
+	private Empleado empleado =new Empleado() ;
 	
 	String provincias[] = {"Jujuy","Buenos Aires","Catamarca","Chaco","Chubut"
 			,"Córdoba","Córdoba","Entre Ríos","Formosa","La Pampa","La Rioja","Mendoza","Misiones","Neuquén"
@@ -22,7 +23,7 @@ public class UsuarioController {
 
 	@GetMapping("/NuevoUsu")
 	public ModelAndView getFormEmpleado() {
-		ModelAndView mav = new ModelAndView("usuario");
+		ModelAndView mav = new ModelAndView("nuevo_empleado");
 		mav.addObject("empleado", empleado);
 		return mav;
 	}
