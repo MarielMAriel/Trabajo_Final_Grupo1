8da249c2f575fa8900241000027fd005eedfdf36
gusta <gusta@LAPTOP-P47OBGCS>
2022-06-16 09:23:22 -0300

    formulario empleado funcionando retorno de lo cargado en BD en listaempleados

--- Proyecto_Final_G1/src/main/java/fi/unju/edu/ar/service/IEmpleadoService.java
+++ Proyecto_Final_G1/src/main/java/fi/unju/edu/ar/service/IEmpleadoService.java
@@ -1,8 +1,12 @@
 package fi.unju.edu.ar.service;
 
+import java.util.List;
+
 import fi.unju.edu.ar.entity.Empleado;
 
 public interface IEmpleadoService {
+	public Empleado getEmpleado();
 	public void guardarEmplado(Empleado empleado);
+	public List<Empleado>getListaEmpleado();
 		
 }
