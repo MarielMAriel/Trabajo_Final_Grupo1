a6359de03e24740290e8c8969566e506f189da90
melan <melan@DESKTOP-JIO3JRI>
2022-06-17 01:57:19 -0300

    edicion

--- Proyecto_Final_G1/src/main/java/fi/unju/edu/ar/controller/EmpresaController.java
+++ Proyecto_Final_G1/src/main/java/fi/unju/edu/ar/controller/EmpresaController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,5 +22,6 @@ public class EmpresaController {
 		ModelAndView mav=new  ModelAndView("index");
 		return mav;
 	}
+
 	
 }
