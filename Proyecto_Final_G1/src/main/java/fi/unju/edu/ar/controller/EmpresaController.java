a21a7a3117e1ce3f942bdbe7a07588df84860d4b
Emm4nue1 <ema.mendoza97@outlook.com>
2022-07-03 21:13:38 -0300

    modificaciones de redireccion, deslogeo y diseño de navs.

--- Proyecto_Final_G1/src/main/java/fi/unju/edu/ar/controller/EmpresaController.java
+++ Proyecto_Final_G1/src/main/java/fi/unju/edu/ar/controller/EmpresaController.java
@@ -18,9 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
